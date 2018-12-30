748f32048f2c8c1bd574a4f1174395c60d6d70c1
upura <upura0@gmail.com>
2018-12-30 11:21:19 +0900

    use pytest and fix flake8

--- test/test_knnFeat.py
+++ test/test_knnFeat.py
@@ -1,18 +1,14 @@
-import unittest
 import numpy as np
-import sys, os
-sys.path.append(os.getcwd())
+import sys
+import os
 from knnFeat import knnExtract
+sys.path.append(os.getcwd())
 
 
-class TestKnnFeat(unittest.TestCase):
-    def test_knnFeat(self):
-        X = np.reshape(np.array([0, 1, 3, 4, 5, 6, 1, 1, 0, 3]), (5, 2))
-        y = np.array([0, 0, 0, 1, 1])
+def test_knnFeat():
+    X = np.reshape(np.array([0, 1, 3, 4, 5, 6, 1, 1, 0, 3]), (5, 2))
+    y = np.array([0, 0, 0, 1, 1])
 
-        expected = knnExtract(X, y, k = 1, holds = 5)
+    expected = knnExtract(X, y, k=1, holds=5)
 
-        self.assertEqual(expected.shape, X.shape)
-
-if __name__ == "__main__":
-    unittest.main()
+    assert expected.shape == X.shape
